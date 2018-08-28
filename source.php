f61f33f15f01c9ef143c188a5d73108ab93d482a
Salman Qureshi <isalmanqureshi@gmail.com>
2018-08-28 23:26:30 +0530

    Add files via upload

---
+++ source.php
@@ -0,0 +1,14 @@
+<?php
+
+$data = $_POST['val'];
+
+//simple php program to show wheather number is even or odd to demonstrate AJAX calls
+
+if($data % 2 == 0){
+  echo "The Given Number is EVEN";
+}
+else {
+  echo "The Given Number is ODD";
+}
+
+ ?>
